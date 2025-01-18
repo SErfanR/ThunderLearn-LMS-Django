e2110e8c7510cfe7159f29c4f1c8d9ef828ae648
serfanr <serfanr.python@gmail.com>
2025-01-18 14:25:17 +0330

    some changes on Classroom and a little part of Choice

--- ThunderLearn/LMS/admin.py
+++ ThunderLearn/LMS/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Classroom
+from .models import Classroom, Choice
 
 admin.site.register(Classroom)
+admin.site.register(Choice)
--- ThunderLearn/LMS/models.py
+++ ThunderLearn/LMS/models.py
@@ -3,10 +3,31 @@ from django.contrib.auth.models import User
 
 
 class Classroom(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=100, unique=True)
     teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='teacher_class')
-    students = models.ManyToManyField(User, related_name='student_class')
+    students = models.ManyToManyField(User, related_name='student_class', blank=True)
+    assistants = models.ManyToManyField(User, related_name='assistant_class', blank=True)
 
     def add_student(self, user):  # adding a student (a user in student group to classroom)
-        if user.groups.filter(name='student').exists():
-            self.students.add(user)
+        if user.groups.filter(name='Students').exists():
+            self.students.add(user)  # TODO: return and else statement
+
+    def remove_student(self, user):  # removing a student
+        try:  # if the student is in self.student remove it else pass
+            self.students.remove(user)
+        finally:
+            pass  # TODO: return and except statement
+
+    def add_assistant(self, user):
+        if user.groups.filter(name='Teachers').exists() or user.groups.filter(name='Assistants').exists():
+            self.assistants.add(user)  # TODO: return and else statement
+
+    def remove_assistant(self, user):
+        try:
+            self.assistants.remove(user)
+        finally:
+            pass  # TODO: return and except statement
+
+
+class Choice(models.Model):
+    body = models.TextField()

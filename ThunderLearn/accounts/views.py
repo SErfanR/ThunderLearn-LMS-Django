9a15664ea0e5073d2541157c2c67e0de7b632a01
serfanr <serfanr.python@gmail.com>
2025-04-14 15:12:16 +0330

    Registration system

--- ThunderLearn/accounts/views.py
+++ ThunderLearn/accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .forms import RegisterForm
+from django.contrib.auth import login
 
 
 class UserLoginView(LoginView):
@@ -13,3 +15,17 @@ class UserLoginView(LoginView):
     def form_invalid(self, form):
         messages.error(self.request, 'نام کاربری یا رمز عبور اشتباه است')
         return self.render_to_response(self.get_context_data(form=form))
+
+
+class RegisterView(FormView):
+    template_name = 'accounts/register.html'
+    form_class = RegisterForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+
+        return super(RegisterView, self).form_valid(form)
